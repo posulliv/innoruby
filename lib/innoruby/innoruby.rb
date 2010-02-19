ac58a2592409b1c43a672869a4ca56902df7463a
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2010-02-19 10:27:48 -0500

    Added simple startup and shutdown methods to InnoDB class. Still need to add error checking

--- lib/innoruby/innoruby.rb
+++ lib/innoruby/innoruby.rb
@@ -5,10 +5,19 @@ The Embedded InnoDB class.
 class InnoDB
 
   def initialize()
+    Lib.ib_init()
   end
 
   def version()
     @ver = Lib.ib_api_version
+  end
+
+  def startup()
+    Lib.ib_startup("barracuda")
+  end
+
+  def shutdown()
+    Lib.ib_shutdown()
   end
 
 end
