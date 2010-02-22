31ffc0ce5e067cd0750fe3d197bb61527e5aa96c
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2010-02-22 11:46:49 -0500

    Started working on adding simple tests.

--- test/test_innoruby.rb
+++ test/test_innoruby.rb
@@ -1,7 +1,25 @@
 require 'helper'
 
 class TestInnoruby < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def setup
+    @inno = InnoDB.new
   end
+
+  should "start an InnoDB instance" do
+    @inno.startup
+  end
+
+  should "stop an InnoDB instance" do
+    @inno.shutdown
+  end
+
+  should "create a database" do
+    @inno.create_database("padraig")
+  end
+
+  should "drop a database" do
+    @inno.drop_database("padraig")
+  end
+
 end
